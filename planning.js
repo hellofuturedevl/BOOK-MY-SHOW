1afef1fa4c163c151723b8bba7fcefd37db22f22
hellofuturedevl <90560207+hellofuturedevl@users.noreply.github.com>
2021-10-20 12:57:28 +0530

    Add files via upload

---
+++ planning.js
@@ -0,0 +1,24 @@
+//book my show planning
+
+// -> we need a reuable carousel
+//-> HREO CAROUSEL
+//-> COMMON CAROUSEL
+//-> CAST CAROUSEL (samll carousel)
+
+// pages
+
+//-> home page
+//-> movie page
+//-> categories page
+//-> auth page
+//-> payment page
+
+//plan how we are building it ?
+
+//Layouts
+//routes
+//pages
+//components
+
+
+
